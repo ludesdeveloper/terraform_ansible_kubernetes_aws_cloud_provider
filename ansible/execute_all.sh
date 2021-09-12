81843dfb70d38b30e6778297a3fe586bed664f97
FunGuardian <seski.ramadhan@gmail.com>
2021-09-13 02:23:34 +0700

    remove initial.yml

--- ansible/execute_all.sh
+++ ansible/execute_all.sh
@@ -1,5 +1,4 @@
 ansible all -m ping
-ansible-playbook -i hosts initial.yml
 ansible-playbook -i hosts kube-dependencies.yml
 ansible-playbook -i hosts master.yml
 ansible-playbook -i hosts workers.yml
