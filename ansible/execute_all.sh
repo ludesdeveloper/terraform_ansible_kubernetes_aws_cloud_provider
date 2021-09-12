6c7082aed8fa56bb95958be6608ef7cda243fc0c
FunGuardian <seski.ramadhan@gmail.com>
2021-09-13 00:36:56 +0700

    adding ansible files

---
+++ ansible/execute_all.sh
@@ -0,0 +1,5 @@
+ansible all -m ping
+ansible-playbook -i hosts initial.yml
+ansible-playbook -i hosts kube-dependencies.yml
+ansible-playbook -i hosts master.yml
+ansible-playbook -i hosts workers.yml
